7aa01e4cf407856ed370d69d8ef8b93ff413838b
Shariq Ahmed <shariq.ahmed525@gmail.com>
2020-01-06 01:42:40 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+  const [edit, setEdit] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const submit = e => {
+    e.preventDefault();
+    setTodos([...todos, text])
+    setText('')
+  }
+
+  const update = e => {
+    e.preventDefault();
+    todos[index] = text;
+    setTodos([...todos])
+    setText('');
+    setEdit(false);
+    setIndex('');
+  }
+
+  const handleEdit = (i, v) => {
+    setEdit(true);
+    setIndex(i);
+    setText(v);
+  }
+
+  const handleDelete = (i) => {
+    todos.splice(i, 1);
+    setTodos([...todos]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <form onSubmit={edit ? update : submit}>
+        <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        <input type="submit" value={edit ? 'Udpate' : 'Add'} />
+      </form>
+      <ul>
+        {todos.map((v, i) => {
+          return (
+            <li key={i}>
+              {v} {' '}
+              <button onClick={() => handleEdit(i, v)}>Edit</button>
+              <button onClick={() => handleDelete(i)}>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
